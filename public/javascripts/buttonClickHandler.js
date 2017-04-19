8576c2184e0b09ee1b4e54eebc7908eedfe3c797
James Sangalli <j.l.sangalli@gmail.com>
2017-04-19 16:57:15 +1000

    add index to html button and param input to track each other, stash changes for the day

--- public/javascripts/buttonClickHandler.js
+++ public/javascripts/buttonClickHandler.js
@@ -5,11 +5,14 @@ let request = require("superagent");
 
 $(function()
 {
-    //TODO get params
+    //TODO get id number from name and match it to params id number
+    //TODO then use that to get param and send it to server
     $(':button').click(function(e)
     {
         let abi = $("#ABI").val();
         let contractAddress = $("#contractAddress").val();
+        let param = "";
+
         if(abi == "" || contractAddress == "")
         {
             alert("missing abi and/or contract");
