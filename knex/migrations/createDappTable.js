04ab33c03b5b83d519b32cdaf89c6a3ddab0d8e4
James Sangalli <j.l.sangalli@gmail.com>
2017-04-25 16:14:42 +1000

    add knex migration, modules and seed

---
+++ knex/migrations/createDappTable.js
@@ -0,0 +1,18 @@
+exports.up = function(knex, Promise)
+{
+    console.log('create dApp table');
+
+    return knex.schema.createTableIfNotExists('dAppTable', function(table) {
+        table.increments('id');
+        table.string('dAppName');
+        table.string('abi');
+        table.string('contractAddress');
+    })
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('dAppTable').then(function ()
+    {
+        console.log('dAppTable table was dropped');
+    })
+};
