d0c84e83d0282cb602f2bb94839b089a36e49f18
James Sangalli <j.l.sangalli@gmail.com>
2017-04-29 13:48:44 +1000

    update seed

--- knex/seeds/seed.js
+++ knex/seeds/seed.js
@@ -2,8 +2,8 @@ exports.seed = function(knex, Promise)
 {
     return Promise.join(
         // Deletes ALL existing entries
-        knex('dAppTable').del(),
+        knex('dappname').del(),
         // Inserts seed entries
-        knex('dAppTable').insert({dappname: 'FoundationTipJar', contractaddress: "0xfb6916095ca1df60bb79ce92ce3ea74c37c5d359"})
+        knex('dappname').insert({dappname: 'FoundationTipJar', contractaddress: "0xfb6916095ca1df60bb79ce92ce3ea74c37c5d359"})
     );
 };
