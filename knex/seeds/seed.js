0ab90318fc80132c88c41ef9680254b658c27497
James Sangalli <j.l.sangalli@gmail.com>
2017-04-29 13:50:44 +1000

    fix typo in seed

--- knex/seeds/seed.js
+++ knex/seeds/seed.js
@@ -2,8 +2,8 @@ exports.seed = function(knex, Promise)
 {
     return Promise.join(
         // Deletes ALL existing entries
-        knex('dappname').del(),
+        knex('dapptable').del(),
         // Inserts seed entries
-        knex('dappname').insert({dappname: 'FoundationTipJar', contractaddress: "0xfb6916095ca1df60bb79ce92ce3ea74c37c5d359"})
+        knex('dapptable').insert({dappname: 'FoundationTipJar', contractaddress: "0xfb6916095ca1df60bb79ce92ce3ea74c37c5d359"})
     );
 };
