ebc1b5d02199dcd39e35cff0345d8f80a0c531af
James Sangalli <j.l.sangalli@gmail.com>
2017-06-22 14:36:29 +1000

    add etherscan button

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,9 @@ router.get('/', (req, res, next) => {
 
 router.get("/api/:contractAddress", (req,res,next) => {
 
+    //TODO should not display if not mainnet
     let contractAddress = req.params.contractAddress;
+    let etherscanURL = "https://etherscan.io/address/" + contractAddress;
 
     web3Handler.checkIfContractIsVerified(contractAddress, (err, data) =>
     {
@@ -19,7 +21,8 @@ router.get("/api/:contractAddress", (req,res,next) => {
                 abiError: "PLEASE PASTE ABI HERE",
                 addressVal: contractAddress,
                 functionTitle:"Smart Contract Functions",
-                warning:"NO ABI FOUND AS CONTRACT IS NOT VERIFIED ON ETHERSCAN"
+                warning:"NO ABI FOUND AS CONTRACT IS NOT VERIFIED ON ETHERSCAN",
+                etherscanURL : etherscanURL
             });
         }
         else
@@ -40,6 +43,8 @@ router.get('/api/:abi/:address', (req,res,next) => {
     //function and param names
     let functionNameAndParamObj = web3Handler.getContractFunctionNamesAndParams(abiFunctions);
     //sets up function calls to contract from UI
+    //TODO should not display if not mainnet
+    let etherscanURL = "https://etherscan.io/address/" + contractAddress;
 
     web3Handler.checkIfContractIsVerified(contractAddress, (error, data) =>
     {
@@ -52,7 +57,8 @@ router.get('/api/:abi/:address', (req,res,next) => {
                 functionParams : functionNameAndParamObj.params,
                 functionTitle:"Smart Contract Functions",
                 warning:"Warning! Contract source code is not verified on etherscan!",
-                readOnlyAttribute: functionNameAndParamObj.readOnly
+                readOnlyAttribute: functionNameAndParamObj.readOnly,
+                etherscanURL : etherscanURL
             });
         }
         else
@@ -64,7 +70,8 @@ router.get('/api/:abi/:address', (req,res,next) => {
                 functionParams : functionNameAndParamObj.params,
                 functionTitle:"Smart Contract Functions",
                 warning:"Contract source code is verified on etherscan!",
-                readOnlyAttribute: functionNameAndParamObj.readOnly
+                readOnlyAttribute: functionNameAndParamObj.readOnly,
+                etherscanURL : etherscanURL
             });
         }
     });
