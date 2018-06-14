7425ed62508b35f92a6787fe1d2fb4359f506674
James Sangalli <j.l.sangalli@gmail.com>
2018-06-14 16:21:56 +0800

    slight clean up

--- routes/search.js
+++ routes/search.js
@@ -29,7 +29,7 @@ router.get("/search/:dappname", (req,res,next) =>
 
         }
 
-        for(result of data)
+        for(let result of data)
         {
             console.log("dApps found");
             let resultObj = {};
